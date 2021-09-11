1424d556d84331a624133a0af51b514422946139
williamlude <90511254+williamlude@users.noreply.github.com>
2021-09-11 08:08:56 -0500

    Ene

---
+++ GitHub Ejemplo2.py
@@ -0,0 +1,12 @@
+#variables de tipo cadena
+nombre = "Jhonatan"
+ape = 'Abal'
+nomape = nombre + ' ' + ape
+#imprimir cadenas
+print(nombre)
+print(ape)
+print(nomape)
+#visualizar tipo de dato
+print(type(nombre))
+print(type(ape))
+print(type(nomape))
